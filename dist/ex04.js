071c70b11978faf0cf1f6c04d6a68bfc8fe6df4e
Miya Kuo <miya3927@gmail.com>
2025-10-01 11:44:18 -0700

    Finished exercise

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,17 +1,16 @@
+"use strict";
 // 4. Create a function named `findMax` that takes an array of numbers as a parameter.
 //    The function should return the largest number in the array.
 //    Ensure the parameter and return type are typed.
-
-function findMax(numbers: number[]): number {
-    let max = numbers[0]
+function findMax(numbers) {
+    let max = numbers[0];
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > max) {
-            max = numbers[i]
+            max = numbers[i];
         }
     }
-    return max
+    return max;
 }
-
 // Expected output:
 console.log(findMax([1, 5, 3, 9, 2])); // 9
-console.log(findMax([7, 2, 10, 4]));   // 10
+console.log(findMax([7, 2, 10, 4])); // 10
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,18 +1,17 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string): number {
-    const wordArr = word.toLowerCase().split('')
-    let count  = 0
+function countVowels(word) {
+    const wordArr = word.toLowerCase().split('');
+    let count = 0;
     for (let i = 0; i < wordArr.length; i++) {
         if (wordArr[i] === 'a' || wordArr[i] === 'e' || wordArr[i] === 'i' || wordArr[i] === 'o' || wordArr[i] === 'u') {
-            count++
+            count++;
         }
     }
-    return count
+    return count;
 }
-
 // Expected output:
-console.log(countVowels("hello"));    // 2
-console.log(countVowels("typescript"));// 2
+console.log(countVowels("hello")); // 2
+console.log(countVowels("typescript")); // 2
